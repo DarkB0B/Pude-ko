0c1a3a45937f2b489511ea79ac8238956118eb54
DarkB0B <miecze123@onet.pl>
2022-05-04 15:07:05 +0200

    work in progress

---
+++ pudelko/Pudelko.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pudelko
+{
+    public sealed class Pudelko
+    {
+        public double A { get; private set;}
+        public double B { get; private set; }
+        public double C { get; private set; }
+        public Pudelko(double a, double b, double c, UnitOfMeasure unit = UnitOfMeasure.meter)
+        {
+            if(a <= 0 || b <= 0 || c <= 0 || a > 10 || b > 10 || c > 10)
+            {
+                throw new ArgumentException("błędne wymiary pudełka");
+            }
+        }
+    }
+    
+}
